e8873c19938cc7cb578e2d8ea3f38338f40aed32
Joana <asiakom24@vp.pl>
2021-09-28 17:20:09 +0200

    ?

--- exp_1_plot.py
+++ exp_1_plot.py
@@ -1,13 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from methods import dderror
+import e1_config
 
-res_clf = np.load('results_ex1/clf_5feat_5drifts_sudden_1subspace_size.npy')
-res_arr = np.load('results_ex1/drf_arr_5feat_5drifts_sudden_1subspace_size.npy')
+res_clf = np.load('results_ex1/clf_15feat_5drifts_sudden_1subspace_size.npy')
+res_arr = np.load('results_ex1/drf_arr_15feat_5drifts_sudden_1subspace_size.npy')
 
 print(res_clf.shape) #replications x threshold x detectors 
 print(res_arr.shape) #replications x threshold x detectors x (real, detected) x chunks-1
 
+th_arr = e1_config.e1_drf_threshold()
+det_arr = e1_config.e1_n_detectors()
+
+print(th_arr, det_arr)
 
 res_clf_mean = np.mean(res_clf, axis=0)
 print(res_clf_mean)
@@ -26,31 +31,32 @@ for rep in range(res_arr.shape[0]):
 
 res_arr_mean = np.mean(dderror_arr, axis=0)
 
-plt.imshow(res_arr_mean)
-plt.savefig('foo.png')
+# plt.imshow(res_arr_mean)
+# plt.savefig('foo.png')
 
-# fig, ax = plt.subplots(1, 1, figsize=(8, 8))
-# ax.set_title('clf_%if_100ch_150chs_5d seed:654' % f)
-# # ax.set_title('%if_100ch_150chs_5d seed:654' % f)
 
-# ax.imshow(res_clf_mean, cmap='cividis')
 
-# ax.set_yticks(list(range(len(subspace_sizes))))
-# ax.set_yticklabels(subspace_sizes)
-# ax.set_ylabel("Subspace size")
 
-# ax.set_xticks(list(range(len(n_detectors))))
-# ax.set_xticklabels(n_detectors)
-# ax.set_xlabel("n detectors")
+fig, ax = plt.subplots(1, 1, figsize=(8, 8))
 
-# for _a, __a in enumerate(subspace_sizes):
-#     for _b, __b in enumerate(n_detectors):
-#         ax.text(_b, _a, "%.3f" % (
-#             res_mean[_a, _b]) , va='center', ha='center', c='white', fontsize=11)
+ax.imshow(res_clf_mean, cmap='cividis')
+
+ax.set_yticks(list(range(len(th_arr))))
+ax.set_yticklabels(th_arr)
+ax.set_ylabel("Threshold")
+
+ax.set_xticks(list(range(len(det_arr))))
+ax.set_xticklabels(det_arr)
+ax.set_xlabel("n detectors")
+
+for _a, __a in enumerate(th_arr):
+    for _b, __b in enumerate(det_arr):
+        ax.text(_b, _a, "%.3f" % (
+            res_clf_mean[_a, _b]) , va='center', ha='center', c='white', fontsize=11)
             
 
-# plt.tight_layout()
-# plt.savefig("figures/clf_%if_100ch_150chs_5d seed_654.png" % f)
-# # plt.savefig("figures/%if_100ch_150chs_5d seed_654.png" % f)
+plt.tight_layout()
+plt.savefig('foo.png')
+# plt.savefig("figures/%if_100ch_150chs_5d seed_654.png" % f)
 
-# plt.close()
+plt.close()
