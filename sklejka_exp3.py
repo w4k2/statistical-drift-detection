3f78cb88d087b2fff759991a2ddbde898bb3804c
Joana <asiakom24@vp.pl>
2022-03-04 19:57:14 +0100

    merge exp3

---
+++ sklejka_exp3.py
@@ -0,0 +1,41 @@
+import e2_config
+import e2_config_hddm
+
+import numpy as np
+
+drf_types = ['cubic', 'nearest']
+detector_names = e2_config.e2_clf_names()
+
+drifts_n = [3,5,7]
+features_n = 15
+
+streams = [
+    'australian',
+    'banknote',
+    'diabetes',
+    'wisconsin',
+]
+
+replications=10
+
+detector_names_original = e2_config.e2_clf_names()
+detector_names_hddm = e2_config_hddm.e2_clf_names()
+
+for str_id, stream in enumerate(streams):
+    #drifts, drf types
+    for d_id, d in enumerate(drifts_n):
+        for d_type_id, d_type in enumerate(drf_types):
+
+            res_arr_original = np.load('results_ex2_real/drf_arr_str_%s.csv_%s_%idrfs.npy' %(stream, d_type, d))
+            res_arr_hddm = np.load('results_ex2_real/drf_arr_str_%s.csv_%s_%idrfs.npy_hddm.npy' %(stream, d_type, d))
+
+            res_clf_original = np.load('results_ex2_real/clf_str_%s.csv_%s_%idrfs.npy' %(stream, d_type, d))
+            res_clf_hddm = np.load('results_ex2_real/clf_str_%s.csv_%s_%idrfs.npy_hddm.npy' %(stream, d_type, d))
+
+            new_clf = np.concatenate((res_clf_original, res_clf_hddm), axis=1)
+            new_arr = np.concatenate((res_arr_original, res_arr_hddm), axis=1)
+
+            np.save('results_ex2_real/drf_arr_str_%s.csv_%s_%idrfs_all.npy' %(stream, d_type, d), new_arr)
+            np.save('results_ex2_real/clf_str_%s.csv_%s_%idrfs_all.npy' %(stream, d_type, d), new_clf)
+
+
