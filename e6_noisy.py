cb061a1fa17b073ea26eb8e4a1f3020326710c28
Joana <asiakom24@vp.pl>
2022-05-02 10:47:49 +0200

    fix e6 (??)

--- e6_noisy.py
+++ e6_noisy.py
@@ -30,20 +30,20 @@ static_params['chunk_size']=250
 
 metrics = e2_config.metrics()
 
-y_flip = [0.0, 0.03, 0.06, 0.09, 0.12, 0.15]
-n_informative = [15,13,11,9,7,5]
+y_flip = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]
+n_redundant = [0,3,6,9,12]
 features=15
 
-t = replications*len(y_flip)*len(n_informative)
+t = replications*len(y_flip)*len(n_redundant)
 pbar = tqdm(total=t)
 
-results_clf = np.zeros((len(y_flip), len(n_informative), replications, static_params['n_chunks']-1))
-results_drf_arrs = np.zeros((len(y_flip), len(n_informative), replications, 2, static_params['n_chunks']-1))
+results_clf = np.zeros((len(y_flip), len(n_redundant), replications, static_params['n_chunks']-1))
+results_drf_arrs = np.zeros((len(y_flip), len(n_redundant), replications, 2, static_params['n_chunks']-1))
 
 real_drf = find_real_drift(static_params['n_chunks'], 2)
 
 for flip_id, flip in enumerate(y_flip):
-    for inf_id, n_inf in enumerate(n_informative):
+    for red_id, n_red in enumerate(n_redundant):
         for replication in range(replications):
 
             detectors = [
@@ -53,23 +53,25 @@ for flip_id, flip in enumerate(y_flip):
             config = {
                 **static_params,
                 'n_features': features, 
-                'n_informative': n_inf,
+                'n_informative': features-n_red,
+                'n_redundant': n_red,
                 'n_drifts': 2,
-                'random_state': random_states[replication]
+                'random_state': random_states[replication],
+                'y_flip':flip
                 }
 
             stream = sl.streams.StreamGenerator(**config)
 
-            print("replication: %i, informative: %i, yflip: %f" % (replication, n_inf, flip))
+            print("replication: %i, redundant: %i, yflip: %f" % (replication, n_red, flip))
 
             eval = sl.evaluators.TestThenTrain(metrics=metrics)
             eval.process(stream, detectors)
 
             scores = eval.scores
-            results_clf[flip_id, inf_id, replication] = scores[:,:,0]
+            results_clf[flip_id, red_id, replication] = scores[:,:,0]
 
-            results_drf_arrs[flip_id, inf_id, replication, 0] = real_drf
-            results_drf_arrs[flip_id, inf_id, replication, 1] = np.array(detectors[0].detector.drift)
+            results_drf_arrs[flip_id, red_id, replication, 0] = real_drf
+            results_drf_arrs[flip_id, red_id, replication, 1] = np.array(detectors[0].detector.drift)
 
             pbar.update(1)
 
--- plt_e6_noisy.py
+++ plt_e6_noisy.py
@@ -11,21 +11,21 @@ from methods import Meta, SDDE
 from sklearn.naive_bayes import GaussianNB
 import matplotlib.pyplot as plt
 
-y_flip = [0.0, 0.03, 0.06, 0.09, 0.12, 0.15]
-n_informative = [15,13,11,9,7,5]
+y_flip = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]
+n_redundant = [0,3,6,9,12]
 
 res = np.load('results_ex6/drf_arr_res.npy') 
 print(res.shape) #y_flip, informative, reps, 2, 49
 
-fig, ax = plt.subplots(6,6,figsize=(13,11))
+fig, ax = plt.subplots(6,5,figsize=(13,11))
 
 for y_f_id, y_f in enumerate(y_flip):
-    for n_in_id, n_in in enumerate(n_informative):
+    for n_in_id, n_in in enumerate(n_redundant):
 
         dets = res[y_f_id, n_in_id, :, 1]
 
         aa = ax[y_f_id, n_in_id]
-        aa.set_title('inf: %.2f, flip: %.2f' % (n_in/15, y_f))
+        aa.set_title('red: %.2f, flip: %.2f' % (n_in/15, y_f))
 
         aa.imshow(dets,
                 vmin=0, vmax=2,
